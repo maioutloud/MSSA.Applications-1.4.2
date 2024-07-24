97d30bb932b40f97abb71e874a1615cf51006943
Mai <135255248+maioutloud@users.noreply.github.com>
2024-07-24 13:09:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Applications_1._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the Class
+
+            Student stu1 = new Student();
+            try
+            {
+                Console.Write("Enter the Student ID : ");
+                stu1.StudentId = int.Parse(Console.ReadLine());
+                Console.Write("Enter the Student First Name : ");
+                stu1.StudentFname = (Console.ReadLine());
+                Console.Write("Enter the Student Last Name : ");
+                stu1.StudentLname = (Console.ReadLine());
+                Console.Write("Enter the Student Grade : (A,B,C,D,F)");
+                stu1.StudentGrade = char.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input. Please enter valid data.");
+            }
+            // Display Entry
+            Console.WriteLine();
+            Console.WriteLine("Student Information");
+            Console.WriteLine($"Student ID: {stu1.StudentId}");
+            Console.WriteLine($"Student First Name: {stu1.StudentFname}");
+            Console.WriteLine($"Student Last Name: {stu1.StudentLname}");
+            Console.WriteLine($"Student Grade: {stu1.StudentGrade}");
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Applications_1._4._2
+{
+    internal class Student
+    {
+        // private *use camel case*
+        private int studentId;
+        private string studentFname;
+        private string studentLname;
+        private char studentGrade;
+
+        // construct to initualize
+        public Student(int id, string fname, string lname, char grade) 
+        {
+            studentId = id;
+            studentFname = fname;
+            studentLname = lname;
+            studentGrade = grade;
+        }
+        // construct
+        public Student()
+        {
+        }
+        // public 
+        public int StudentId
+        {
+            get { return studentId; }
+            set { studentId = value; }
+        }
+        public string StudentFname
+        {
+            get { return studentFname; }
+            set { studentFname = value; }
+        }
+        public string StudentLname
+        {
+            get { return studentLname; }
+            set { studentLname = value; }
+        }
+        public char StudentGrade
+        {
+            get { return studentGrade; }
+            set { studentGrade = value; }
+        }
+    }
+}
